1c93bb4965a8a1104930df18262b91f44c348d5a
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-20 21:06:52 +0200

    initial setup

---
+++ 00-Full-Page/js/main.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+function initNavigation() {
+  const mainNavLinks = gsap.utils.toArray(".main-nav a");
+
+  mainNavLinks.forEach((link) => {
+    link.addEventListener("mouseleave", (e) => {
+      // add class
+      link.classList.add("animate-out");
+      //remove after animate out done, 2. Varianten
+      //   setTimeout(() => {
+      //     link.classList.remove("animate-out");
+      //   }, 300);
+      link.ontransitionend = function () {
+        link.classList.remove("animate-out");
+      };
+    });
+  });
+}
+
+function init() {
+  initNavigation();
+}
+
+window.addEventListener("load", function () {
+  init();
+});
