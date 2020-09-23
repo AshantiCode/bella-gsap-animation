d172a218dc403e11fb9d677ce0656d5faefa4d4d
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-23 21:26:07 +0200

    add text reveal animation

--- 02-Reveal-Gallery/js/main.js
+++ 02-Reveal-Gallery/js/main.js
@@ -9,11 +9,15 @@ function initHoverReveal() {
         column.mask = column.querySelector(".rg__image--mask");
         column.text = column.querySelector(".rg__text");
         column.textCopy = column.querySelector(".rg__text--copy");
+        column.textMask = column.querySelector(".rg__text--mask");
+		column.textP = column.querySelector(".rg__text--copy p");
+		column.image = column.querySelector(".rg__column img")
 
         // reset their initial position (images are not visisble on
         // page load
-        gsap.set(column.imageBlock, { yPercent: -101 });
-        gsap.set(column.mask, { yPercent: 100 });
+        gsap.set([column.imageBlock, column.textMask], { yPercent: -101 });
+		gsap.set([column.mask, column.textP], { yPercent: 100 });
+		gsap.set(column.image, {scale: 1.2})
 
         column.addEventListener("mouseenter", createHoverReveal);
         column.addEventListener("mouseleave", createHoverReveal);
@@ -27,23 +31,24 @@ function getTextCopyHeight(textCopy) {
 function createHoverReveal(e) {
     // console.log(e.target);
 
-    const { imageBlock, mask, text, textCopy } = e.target;
+    const { imageBlock, mask, text, textCopy, textMask, textP, image } = e.target;
 
     let tl = gsap.timeline({
-        defaults: {duration: 0.7, ease: "power4.out",
+        defaults: {duration: 1, ease: "power4.out",
         },
     });
 
     if (e.type === "mouseenter") {
 		tl
-		// .to([mask, imageBlock], {duration: 1, yPercent: 0,})
+		.to([mask, imageBlock, textMask, textP], { yPercent: 0,})
 		.to(text, {y: () => -getTextCopyHeight(textCopy)/ 2}, 0 ) // moves text up half of its height
-			
+		.to(image, {duration: 1.2, scale:1}, 0)
     } else if (e.type === "mouseleave") {
 		tl
-		// .to(mask, { yPercent: 100 })
-		// .to(imageBlock, { yPercent: -101 }, 0)
+		.to([mask, textP], { yPercent: 100 })
+		.to([imageBlock, textMask], { yPercent: -101 }, 0)
 		.to(text, {y: 0},0)
+		.to(image, {scale:1.2}, 0)
     }
 
     return tl;
