68706af33fa408615322f16dad5b2329b94ac6e0
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-24 20:07:05 +0200

    add finished mouse leave, image offset need to be done

--- 03-Portfolio-Hover/js/main.js
+++ 03-Portfolio-Hover/js/main.js
@@ -27,16 +27,16 @@ function hoverEffekt(e) {
 			.to([imageSmall, imageLarge], {autoAlpha: 1}, 0)
 			.to(activeLink, {color: "#fff", autoAlpha: 1}, 0)
 			.to(allSiblings, {color: "#fff", autoAlpha: 0.2}, 0)
-			.to(pageBackground, {backgroundColor: color, duration: 0.5})
+			.to(pageBackground, {backgroundColor: color},0)
 			
-	// changing the backgrounimage/ make images visible (autoalpha?)
-	// change background color
-
 	} else if (e.type === "mouseleave") {
-		const tl = gsap.timeline({defaults: {duration: 1, ease: "power3.out"}})
-		
-	//fade font color back to default
-	//fade background color back two default
+		const tl = gsap.timeline()
+		tl
+			.to([imageSmall, imageLarge], {autoAlpha: 0}, 0)
+			.to(allLinks, {color: "#000", autoAlpha: 1},0)
+			.to(pageBackground, {backgroundColor: "#ACB7AE"})
+
+	
 	}
 }
 
