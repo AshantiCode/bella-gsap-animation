d598f59d9d7bc5225c147be009f4f188e41f0426
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-24 12:33:45 +0200

    change link color on hover, mouseenter

--- 03-Portfolio-Hover/js/main.js
+++ 03-Portfolio-Hover/js/main.js
@@ -1,36 +1,36 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
+const links = gsap.utils.toArray(".portfolio__categories a")
 
 function initLinkHover() {
-	const links = gsap.utils.toArray(".portfolio__categories a")
 	
 	links.forEach(link => {
 		link.addEventListener("mouseenter", hoverEffekt)
 		link.addEventListener("mouseleave", hoverEffekt)
 
 	})
-	// on mouseenter
-	//change font color to pure white
-	//all the others 0.2 opacite
-	// changing the backgrounimage/ make images visible (autoalpha?)
-	// change background color
-
-	//on mouseleave
-	//hiding thw 2 images
-	//fade font color back to default
-	//fade background color back two default
 };
 
 function hoverEffekt(e) {
-	console.log(e.type)
-	if(e.type === mouseenter) {
+	// console.log(e.target)
+	const imgL = document.querySelector(".portfolio__image--l")
+	const imgS = document.querySelector(".portfolio__image--s")
+	const categories = document.querySelector(".portfolio__categories")
+	const activeLink = e.target
+
+	if(e.type === "mouseenter") {
+		const tl = gsap.timeline({defaults: {duration: 1, ease: "power3.out"}})
+
+		tl
+			.to(links, {color: "#fff", opacity: 0.2})
+			.to(activeLink, {color: "#fff", opacity: 1}, 0)
 		//change font color to pure white
 	//all the others 0.2 opacite
 	// changing the backgrounimage/ make images visible (autoalpha?)
 	// change background color
 
-	} else if (e.type === mouseleave) {
+	} else if (e.type === "mouseleave") {
 //hiding thw 2 images
 	//fade font color back to default
 	//fade background color back two default
