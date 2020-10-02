4330c39dd961e7c9c0b7c27c53666b4d46f47260
Ashanti Haring <ashanti.haring@gmail.com>
2020-10-02 12:38:19 +0200

    add images, begin initail loader animation

--- 05-Intro-Animation/js/main.js
+++ 05-Intro-Animation/js/main.js
@@ -1,8 +1,35 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const select = (el) => document.querySelector(el)
+const selectAll = (el) => document.querySelectorAll(el)
+
+function initLoader() {
+
+	const tlLoaderIn = gsap.timeline({
+		defaults: {
+			duration: 1.1,
+			ease: "power2.out",
+		}
+	})
+
+	const loaderInner = select(".loader .inner")
+	const image = select(".loader__image img")
+	const mask = select(".loader__image--mask")
+	const line1 = select(".loader__title--mask:nth-child(1) span")
+	const line2 = select(".loader__title--mask:nth-child(2) span")
+
+	tlLoaderIn
+		.from(loaderInner, {
+			scaleY:0,
+			transformOrigin: "bottom"
+		}, 0.3)
+		.addLabel("revealImage")
+		.from(mask, {yPercent: 100}, "revealImage-=0.6")
+}
+
 function init(){
     
-    // start here
+    initLoader()
 
 }
 
